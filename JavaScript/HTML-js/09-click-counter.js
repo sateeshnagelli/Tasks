1a317523bb3a1499d4ac22fc95d2450ed6e311b8
sateeshnagelli <sateeshnagelli321@gmail.com>
2024-06-18 15:49:23 +0530

    third

---
+++ JavaScript/HTML-js/09-click-counter.js
@@ -0,0 +1,19 @@
+let counterValueEl = document.getElementById("counterValue");
+
+let clickCount = localStorage.getItem("clickCount")
+
+if(clickCount === null){
+    counterValueEl.textContent = 0;
+}
+else{
+    counterValueEl.textContent = clickCount;
+}
+
+function onIncrement(){
+    let previousCounterValue = counterValueEl.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+
+    localStorage.setItem("clickCount", updatedCounterValue);
+
+    counterValueEl.textContent= updatedCounterValue;
+}
